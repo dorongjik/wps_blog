b96894f42d41b3d73460e731b42a7b3e2fce6c23
dorongjik <leemagic12@naver.com>
2017-01-29 18:00:39 +0900

    refactor from views.py to dir view

--- blog/wpsblog/wpsblog/controller.py
+++
@@ -1,8 +0,0 @@
-from django.http.response import HttpResponse
-
-#MVC Controller
-def home(request):
-    return HttpResponse("hello world")
-
-def room(request, room_id):
-    return HttpResponse("This is a room detail " + room_id)
---
+++ blog/wpsblog/wpsblog/views/news.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def news(request):
+	return render(request,
+		"news.html",
+		{"count" : "5"},
+		)
