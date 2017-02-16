9707c2f848bf43a50ffacf7d04064136a97c4720
dorongjik <leemagic12@naver.com>
2017-02-17 00:34:13 +0900

    update models(user info) & mypage

--- wpsblog/wpsblog/models/post.py
+++ wpsblog/wpsblog/models/post.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.urlresolvers import reverse   
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 class PostManager(models.Manager):
     def public(self):
@@ -10,6 +11,7 @@ class Post(models.Model):
 
     objects = PostManager()
 
+    user = models.ForeignKey(User)
     title = models.CharField(
             max_length=120,
     )
