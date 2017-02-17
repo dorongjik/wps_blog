ecad0126896145d45fb9c67907d1c721450d6be5
dorongjik <leemagic12@naver.com>
2017-02-17 20:10:05 +0900

    add pagination

--- wpsblog/wpsblog/views/posts/lists.py
+++ wpsblog/wpsblog/views/posts/lists.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from wpsblog.models import Post
 from wpsblog.models import Comment
 
 def lists(request):
+    page = request.GET.get("page", 1) # 기본값 1(첫 페이지)
+    per = request.GET.get("per", 5) # 몇개씩 할 것인가
+
+    paginator = Paginator(Post.objects.public(), per) # per개씩
+    posts = paginator.page(page)
+
     return render(request,
         "posts/lists.html",
         {
-            "posts": Post.objects.public, # 여기의 posts가 template으로 날아감
+            "posts": posts, # 여기의 posts가 template으로 날아감
         },
         )
