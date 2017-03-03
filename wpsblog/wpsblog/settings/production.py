39f621214fd6720dfe2cafe69a06eb252b4d7c4e
dorongjik <leemagic12@naver.com>
2017-03-03 23:51:05 +0900

    load staticfiles from s3

--- wpsblog/wpsblog/settings/production.py
+++ wpsblog/wpsblog/settings/production.py
@@ -5,5 +5,14 @@ DEBUG = False
 ALLOWED_HOSTS = ["*", ]
 
 
-STATICFILES_STORAGE = 'wpsblog.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'wpsblog.storage.S3PipelineCachedStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+
+AWS_S3_CUSTOM_DOMAIN = 'd1ka1uby1npwpr.cloudfront.net'
+AWS_S3_URL_PROTOCOL = 'https'
+
+STATIC_URL = "https://d1ka1uby1npwpr.cloudfront.net/"
--- wpsblog/wpsblog/storage.py
+++ wpsblog/wpsblog/storage.py
@@ -1,1 +1,12 @@
-storage.py
+from django.contrib.staticfiles.storage import CachedFilesMixin, ManifestFilesMixin
+
+from pipeline.storage import PipelineMixin
+
+from storages.backends.s3boto import S3BotoStorage
+
+class S3PipelineManifestStorage(PipelineMixin, ManifestFilesMixin, S3BotoStorage):
+    pass
+
+class S3PipelineCachedStorage(PipelineMixin, CachedFilesMixin, S3BotoStorage):
+    pass
+
